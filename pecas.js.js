2a171c164e9ce0b63e2fab21c83a0296c081ac4c
EdimiltonD <edimilton.sobrinho@oi.net.br>
2022-06-22 18:29:22 -0300

    Exercicio de pecas feito

---
+++ pecas.js.js
@@ -0,0 +1,21 @@
+let pesoPeca = 200
+let numeroPecas = 150
+let nomePecas = "escapamento"
+
+if(pesoPeca >=150) {
+    console.log("Pode-se cadastrar peca")
+} else { 
+    console.log("Impossivel continuar com o cadastro")
+} 
+
+if ((numeroPecas <= 15)) {
+    console.log("Impossivel cadastrar numero de pecas excedidos")
+} else {
+    console.log("Cadastro permitido")
+}
+
+if (nomePecas.length >=3) {
+    console.log("Numero de caracteres insuficientes")
+} else {
+    console.log("pode cadastrar")
+}
